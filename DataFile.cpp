eea8226a569e456daa5a62319f2c42559eff6f19
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-10-08 11:09:40 -0500

    Create header functions

--- DataFile.cpp
+++ DataFile.cpp
@@ -1,1 +1,33 @@
 #include "DataFile.h"
+
+void DataFile::createFile(char *fileName, unsigned int sizeOfRecordsInBytes) {
+
+}
+
+void DataFile::openFile(char *fileName) {
+
+}
+
+void DataFile::closeFile() {
+
+}
+
+void DataFile::putRecord(long recordNumber, const void *locationToReadFrom) {
+
+}
+
+void DataFile::getRecord(long recordNumber, const void *locationToWriteTo) {
+
+}
+
+int DataFile::recordCount() {
+    return 0;
+}
+
+void DataFile::updateRecordCount(int newRecordCount) {
+
+}
+
+int DataFile::fileStatus() {
+    return 0;
+}
--- DataFile.h
+++ DataFile.h
@@ -1,9 +1,39 @@
 #ifndef DIRECT_ACCESS_FIXED_LENGTH_DATAFILE_H
 #define DIRECT_ACCESS_FIXED_LENGTH_DATAFILE_H
 
+#include <fstream>
+
+#define fsCreateFail 0
+#define fsSuccess 1
+#define fsGetFail 2
+#define fsCloseFail 3
+#define fsOpenFail 4
+#define fsPutFail 5
+
+using std::fstream;
 
 class DataFile {
+private:
+    fstream stream;
+    int recSize;
+    int recCount;
+    int fStatus;
+public:
+    void createFile(char *fileName, unsigned int sizeOfRecordsInBytes);
 
+    void openFile(char *fileName);
+
+    void closeFile();
+
+    void putRecord(long recordNumber, const void *locationToReadFrom);
+
+    void getRecord(long recordNumber, const void *locationToWriteTo);
+
+    int recordCount();
+
+    void updateRecordCount(int newRecordCount);
+
+    int fileStatus();
 };
 
 
